27c2fc4d56c43ab7a6351a2c5ec010fd03b5d0b6
Erfan Arefmehr <erfan.arefmehr@gmail.com>
2023-12-03 16:05:51 +0330

    update(start command): return start state in start command

--- gallery/commands/core/functions.py
+++ gallery/commands/core/functions.py
@@ -1,9 +1,11 @@
 from telegram import Update
 from telegram.ext import ContextTypes
 
+from config.settings import STATES
 from gallery.markups import MAIN_MENU
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     reply_text = 'سلام. به ربات gallery خوش آمدید.'
     await update.message.reply_text(text=reply_text, reply_markup=MAIN_MENU)
+    return STATES.START

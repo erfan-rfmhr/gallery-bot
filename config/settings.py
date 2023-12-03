c9da5b96da84d572800f0a6a568afca4b39f59eb
Erfan Arefmehr <erfan.arefmehr@gmail.com>
2023-12-03 11:52:07 +0330

    feat(states): create bot states

--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,5 @@
+from collections import namedtuple
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -5,5 +7,9 @@ class BotSettings(BaseSettings):
     model_config = SettingsConfigDict(str_strip_whitespace=True, env_file="config/.env")
     TOKEN: str
 
+
+state_names = ('START', 'SEND_NEW_LINK', 'LINKS_IN_QUEUE', 'SEND_NOW', 'SEND_NEWS', 'SET_PUBLIC_HASHTAGS', 'SCHEDULE',)
+
+STATES = namedtuple('states', state_names)(*range(len(state_names)))
 
 settings = BotSettings()

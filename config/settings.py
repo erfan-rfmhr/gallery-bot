1fca727659f403b295721f3bdadda8d160e8eac7
Erfan Arefmehr <erfan.arefmehr@gmail.com>
2023-12-03 15:59:37 +0330

    update(settings): add insta and telegram channel attrs

--- config/settings.py
+++ config/settings.py
@@ -5,7 +5,10 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class BotSettings(BaseSettings):
     model_config = SettingsConfigDict(str_strip_whitespace=True, env_file="config/.env")
-    TOKEN: str
+    TELEGRAM_TOKEN: str
+    TELEGRAM_CHANNEL_ID: str
+    INSTAGRAM_USERNAME: str
+    INSTAGRAM_PASSWORD: str
 
 
 state_names = ('START', 'SEND_NEW_LINK', 'LINKS_IN_QUEUE', 'SEND_NOW', 'SEND_NEWS', 'SET_PUBLIC_HASHTAGS', 'SCHEDULE',)

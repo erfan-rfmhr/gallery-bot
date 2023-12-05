6539042d194f4f49dae3a5e54dfdd9ea15e1a784
Erfan Arefmehr <erfan.arefmehr@gmail.com>
2023-12-05 11:53:41 +0330

    update(perform_immediate_send_handler): set regex filter for urls

--- gallery/handlers/__init__.py
+++ gallery/handlers/__init__.py
@@ -4,4 +4,5 @@ from .core import cancel, immediate_send, perform_immediate_send
 
 cancel_handler = MessageHandler(callback=cancel, filters=filters.Text(strings=['لغو']))
 immediate_send_handler = MessageHandler(callback=immediate_send, filters=filters.Text(strings=['ارسال فوری']))
-perform_immediate_send_handler = MessageHandler(callback=perform_immediate_send, filters=filters.TEXT)
+perform_immediate_send_handler = MessageHandler(callback=perform_immediate_send,
+                                                filters=filters.Regex(pattern=r'^https?://.*'))

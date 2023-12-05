7ca60042ff7653d5df29a166de7ae748d8baf017
Erfan Arefmehr <erfan.arefmehr@gmail.com>
2023-12-05 15:59:46 +0330

    feat(post service): add facebook method

--- services/post.py
+++ services/post.py
@@ -1,25 +1,35 @@
-from instabot import Bot as InstaBot
+# from instabot import Bot as InstaBot
+import httpx
 from telegram import Bot as TelegramBot
 
 from config.settings import settings
 
 
 class PostService:
-    insta_bot = InstaBot()
-    insta_username = settings.INSTAGRAM_USERNAME
-    insta_password = settings.INSTAGRAM_PASSWORD
+    # insta_bot = InstaBot()
+    # insta_username = settings.INSTAGRAM_USERNAME
+    # insta_password = settings.INSTAGRAM_PASSWORD
 
-    telegram_bot = TelegramBot(token=settings.TELEGRAM_TOKEN)
     telegram_channel_id = settings.TELEGRAM_CHANNEL_ID
 
     def __init__(self, caption: str):
         self.caption = caption
 
-    def upload_to_instagram(self, filename: str):
-        bot = self.insta_bot
-        bot.login(username=self.insta_username, password=self.insta_password, use_cookie=False)
-        bot.upload_photo(photo=filename, caption=self.caption)
+    # async def upload_to_instagram(self, filename: str):
+    #     bot = self.insta_bot
+    #     bot.login(username=self.insta_username, password=self.insta_password, use_cookie=False)
+    #     bot.upload_photo(photo=filename, caption=self.caption)
 
     async def upload_to_telegram(self, filename: str):
-        await self.telegram_bot.send_photo(chat_id=self.telegram_channel_id, photo=open(filename, 'rb'),
-                                           caption=self.caption)
+        bot = TelegramBot(token=settings.TELEGRAM_TOKEN)
+        await bot.send_photo(chat_id=self.telegram_channel_id, photo=open(filename, 'rb'),
+                             caption=self.caption, connect_timeout=100, pool_timeout=100, read_timeout=100, write_timeout=100)
+        await bot.close()
+
+    async def upload_to_facebook(self):
+        async with httpx.AsyncClient() as client:
+            payload = {
+                'message': self.caption,
+                'access_token': settings.FACEBOOK_TOKEN
+            }
+            await client.post(url=settings.FACEBOOK_URL, data=payload)
